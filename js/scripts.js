d8ec6f0ddab17de8f324232d2ca210e348044f2c
Pricila Lizcano and Sarah Angel <sarah@>
2019-03-18 20:10:13 -0700

    add more business logic to prototype method in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ function Tickets(movie, time, age) {
 
 Tickets.prototype.cost = function(){
   var cost = 0
-  if (this.age >= 65 || this.time <= 3 || this.time > 16 && this.age >= 65) {
+  if ((this.age >= 65 && this.movie > 1 && this.time <= 3) || (this.age < 65 && this.movie > 1 && this.time <= 3) || (this.movie > 1 && this.time > 3 && this.age >= 65)) {
     return cost + 5
-  } else if(this.movie === 1 && this.age >= 65) {
+  } else if((this.movie === 1 && this.age >= 65 && this.time > 3) || (this.movie ===1 && this.age < 65 && this.time <= 3)) {
     return cost + 10
   } else {
     return cost + 12
@@ -23,8 +23,8 @@ $(document).ready(function() {
 
 
     var ageInput = parseInt($('input#age').val());
-    var movieInput = parseInt($('input#movie').val());
-    var timeInput = parseInt($('input#time').val());
+    var movieInput = parseInt($('#movie').val());
+    var timeInput = parseInt($('#time').val());
     var newTickets = new Tickets(movieInput, timeInput, ageInput);
 
     $('.price').text(newTickets.cost());
